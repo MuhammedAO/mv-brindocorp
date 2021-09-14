2aa194e97c1e8e976fe966643ba3cb1bd7406b2e
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-09-14 15:15:26 +0100

    epic

--- server/routes/epic.js
+++ server/routes/epic.js
@@ -5,18 +5,9 @@ const axios = require("axios")
 const router = express.Router()
 
 
-// router.post("/auth/epicgames", async (req, res) => {
-//   const  code  = req.query.code
-  
-//   console.log(access_token)
-// })
-
-
-
 router.get('/', (req, res) => {
   const code = req.query.code
   if(code) {
-    var output = code
     // res.send(code)
     res.redirect(
       `http://localhost:4000/auth/epic/redirect?code=${code}`
@@ -26,6 +17,7 @@ router.get('/', (req, res) => {
     var output = 'hello'
   }
   console.log(output)
+  res.end()
 })
 
 
